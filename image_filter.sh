c2b51efa6b6b60d947c98b41ca80cd07b54395af
HYmian <gopher.mian@outlook.com>
2017-11-15 01:05:31 +0800

    update: regex

--- image_filter.sh
+++ image_filter.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-image_domain=.*
-image_project=.*
-image_repository=.*
-image_tag=.*
+image_domain=[!/]*/
+image_project=[!/]*/
+image_repository=[!/]*:
+image_tag=[!:]*
 
 while getopts ":d:p:r:t:" opt; do
   case $opt in
     d)
       echo "-d was triggered, Parameter: $OPTARG" >&2
-      image_domain=$OPTARG/
+      image_domain=[!/]*$OPTARG[!/]*/
       ;;
     p)
       echo "-p was triggered, Parameter: $OPTARG" >&2
-      image_project=$OPTARG/
+      image_project=[!/]*$OPTARG[!/]*/
       ;;
     r)
       echo "-r was triggered, Parameter: $OPTARG" >&2
-      image_repository=$OPTARG:
+      image_repository=[!/]*$OPTARG[!/]*:
       ;;
     t)
       echo "-t was triggered, Parameter: $OPTARG" >&2
-      image_tag=$OPTARG$
+      image_tag=.*$OPTARG.*
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -34,7 +34,7 @@ while getopts ":d:p:r:t:" opt; do
   esac
 done
 
-filter=^.*${image_domain}.*${image_project}.*${image_repository}.*${image_tag}
+filter="^(${image_domain})(${image_project})(${image_repository})(${image_tag})$"
 echo ${filter}
 
 local_images=`docker images --format '{{ .Repository }}:{{ .Tag }}'`
@@ -42,5 +42,10 @@ for local_image in $local_images
 do
 	if [[ $local_image =~ $filter ]]; then
 		echo $local_image
+    echo ${#BASH_REMATCH[@]}
+    echo ${BASH_REMATCH[1]}
+    echo ${BASH_REMATCH[2]}
+    echo ${BASH_REMATCH[3]}
+    echo ${BASH_REMATCH[4]}
 	fi
 done
