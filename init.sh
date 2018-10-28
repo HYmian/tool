f4bb9f544c036ed1e39fd448c729876c42bc39cb
HYmian <gopher.mian@gmail.com>
2018-10-28 22:23:53 +0800

    fix

--- init.sh
+++ init.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 
-// config system
+# config system
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
-// add repo
+# add repo
 
-// chrome
-dnf install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-// kubectl
+# kubectl
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -19,18 +17,15 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 exclude=kube*
 EOF
-// vs code
+# vs code
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-// rpm fusion
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# rpm fusion
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-dnf makecache
+dnf makecache -y
 
-// install oh-my-zsh
-dnf install zsh && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-// install common tools
+# install common tools
 dnf install -y \
 	kde-l10n-zh_CN \
 	vim \
@@ -41,10 +36,15 @@ dnf install -y \
 	vlc \
 	libreoffice \
 	code \
-	chrome \
 	steam
 
-// install devlop tools
+# install devlop tools
 dnf install -y \
 	golang golang-docs \
 	java-1.8.0-openjdk-devel
+
+# chrome
+dnf install -y https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+
+# install oh-my-zsh
+dnf install zsh -y && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
