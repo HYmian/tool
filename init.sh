1f1b6bb4eaf58d603b7badca4e89bec757f059d6
HYmian <gopher.mian@gmail.com>
2018-10-29 21:50:12 +0800

    add: comment

--- init.sh
+++ init.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
 # config system
+echo "disable selinux"
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
 # add repo
 
 # kubectl
+echo "add kubernetes repo"
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -18,11 +20,14 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cl
 exclude=kube*
 EOF
 # vs code
+echo "add vs code repo"
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 # rpm fusion
+echo "fusion repo"
 dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+echo "makecache"
 dnf makecache -y
 
 # install common tools
@@ -44,7 +49,9 @@ dnf install -y \
 	java-1.8.0-openjdk-devel
 
 # chrome
+echo "install chrome"
 dnf install -y https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 
 # install oh-my-zsh
+echo "install oh-my-zsh"
 dnf install zsh -y && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
